3751dd49afed17f1a1cbeed59194de9d6f7775a7
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2014-05-31 21:42:40 -0400

    'streaming'

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    io = require('socket.io').listen(server)
+    io = require('socket.io').listen(server),
+    camera = require('./lib/camera').createCamera()
 
 app.use(express.static(__dirname + '/static'))
 app.use(express.static(__dirname + '/images'))
@@ -11,3 +12,13 @@ app.set('port', process.env.PORT || '8888')
 server.listen(app.settings.port, function(){
   console.log('App server listening at http://localhost:'+app.settings.port)
 })
+
+var capture = function(){
+  io.sockets.emit('capture', {name: 'image.jpg'})
+  camera.capture({
+    cwd: __dirname + '/static',
+    out: 'image.jpg'
+  })
+}
+
+camera.on('capture', capture)
---
+++ static/scripts.js
@@ -0,0 +1,13 @@
+angular.module('PiCam',[])
+.controller('PiCamCtrl', function($scope){
+  $scope.data = {}
+  window.socket = $scope.socket = io.connect()
+
+
+  $scope.socket.on('capture', function (capture) {
+    $scope.imageSrc = capture.name+"?t="+Date.now()
+  })
+
+  $scope.imageSrc = 'p1.jpg'
+
+})
